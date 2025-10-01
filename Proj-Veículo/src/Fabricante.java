227ce41fb04b2f03cb3301ed320a025ee8351b4e
LuizGuilherme91 <luizgl_jatai@outlook.com>
2025-10-01 20:33:40 -0300

    Commit-1

---
+++ Proj-Veículo/src/Fabricante.java
@@ -0,0 +1,13 @@
+public class Fabricante {
+    private Integer id;
+    private String nome;
+    private String cnpj;
+    private String cidade;
+
+    public Fabricante(Integer id, String nome, String cnpj, String cidade) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.cidade = cidade;
+    }
+}
